0d33a2aa1b46d35381a5acc4a75735a37a330141
charles-futDevFloripa <charles_doehl@estudante.sesisenai.org.br>
2024-04-18 13:33:07 -0300

    [M1S08] - Ex 2 - Selecionar elemento por classe

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,32 @@
+//exercício 01
 document.addEventListener('DOMContentLoaded', function() {
   const changeBackgroundButton = document.querySelector('#change-background button');
   let isRustyRed = true; 
-
   changeBackgroundButton.addEventListener('click', function() {
       const task = document.getElementById('change-background');
-
       if (isRustyRed) {
           task.style.backgroundColor = 'rgba(80, 138, 168, 0.6)';
       } else {
           task.style.backgroundColor = 'rgba(214, 34, 70, 0.6)';
-      }
-
+              }
       isRustyRed = !isRustyRed; // Alterna o estado
   });
+});
+
+
+//exercício 02
+document.addEventListener('DOMContentLoaded', function() {
+    const changeTextButton = document.querySelector('#change-class button');
+        let originalText = true; // o texto original para fazer o toogle ao clicar no botão
+    changeTextButton.addEventListener('click', function() {
+        const textElements = document.querySelectorAll('.chage-text-in-class'); // Seleciona a classe
+        textElements.forEach(element => {
+            if (originalText) {
+                element.textContent = 'Texto Alterado'; // Muda para o novo texto
+            } else {
+                element.textContent = 'TEXTO'; //volta o texto original
+            }
+        });
+        originalText = !originalText;
+    });
 });
